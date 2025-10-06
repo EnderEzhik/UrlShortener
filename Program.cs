217a3d05e5c3b8c6accaae2946b69d1205c1c9e9
EnderEzhik <Achernov2007@gmail.com>
2025-10-06 15:08:23 +0300

    Обновление структуры базы данных измененно с пересоздания при запуске приложения на работу с миграциями.

--- Program.cs
+++ Program.cs
@@ -14,13 +14,6 @@ public class Program
 
         var app = builder.Build();
 
-        using (var scope = app.Services.CreateScope())
-        {
-            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-        }
-        
         app.MapControllers();
 
         app.Run();
