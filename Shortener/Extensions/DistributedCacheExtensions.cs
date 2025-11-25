2e656462218879dbf56dc2bb45d004595ecc19df
EnderEzhik <Achernov2007@gmail.com>
2025-11-25 23:31:54 +0300

    Добавлены и настроены библиотеки для работы с Redis. Реализованы методы расширения для встроенного кэша.

---
+++ Shortener/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Shortener.Extensions;
+
+public static class DistributedCacheExtensions
+{
+    public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+        string key,
+        T data,
+        TimeSpan? absoluteExpireTime = null,
+        TimeSpan? unusedExpireTime = null)
+    {
+        var options = new DistributedCacheEntryOptions();
+        options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(10);
+        options.SlidingExpiration = unusedExpireTime ??  TimeSpan.FromMinutes(10);
+
+        var jsonData = JsonSerializer.Serialize(data);
+        await cache.SetStringAsync(key, jsonData, options);
+    }
+
+    public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string key)
+    {
+        var jsonData = await cache.GetStringAsync(key);
+
+        if (jsonData is null)
+        {
+            return default(T);
+        }
+        
+        return JsonSerializer.Deserialize<T>(jsonData);
+    }
+}
