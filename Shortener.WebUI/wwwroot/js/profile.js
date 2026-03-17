19a1442fcb6969cd5ff10aad77a7273b66d9df22
EnderEzhik <Achernov2007@gmail.com>
2026-03-17 23:34:28 +0300

    frontend:     add: добавлена загрузка данных профиля в личном кабинете пользователя

--- Shortener.WebUI/wwwroot/js/profile.js
+++ Shortener.WebUI/wwwroot/js/profile.js
@@ -1,5 +1,5 @@
 import {apiServerAddress} from "./config.js";
-import {getAuthToken} from "./common.js";
+import {getAuthToken, buildShortUrl} from "./common.js";
 
 const urlsLoadingIndicator = document.getElementById("links-loading"); // Индикатор загрузки списка ссылок
 const urlsLoadingErrorIndicator = document.getElementById("links-error"); // Индикатор ошибки при неудачной загрузки ссылок
@@ -9,6 +9,15 @@ const urlsEmptyContainer = document.getElementById("links-empty"); // Индик
 const linksList = document.getElementById("links-tbody"); // Сам список ссылок
 
 const urlsContainerElements = document.getElementById("urlsContainer").children; // Индикаторы отображения состояния списка ссылок
+
+const profileContainer = {
+    self: document.getElementById("profile-container"),
+    loadingIndicator: document.getElementById("profile-loading"),
+    errorIndicator: document.getElementById("profile-error"),
+    content: document.getElementById("profile-content"),
+    username: document.getElementById("profile-username"),
+    registrationDate: document.getElementById("profile-registration-date")
+};
 
 function showUrlsContainerElement(element) {
     if (element.classList.contains("d-none")) {
@@ -24,8 +33,52 @@ function showUrlsContainerElement(element) {
     }
 }
 
+function deleteUrlFromUI(shortCode) {
+    const urlToDelete = document.getElementById(shortCode);
+    urlToDelete.remove();
+}
+
+async function deleteUrl(shortCode) {
+    try {
+        const response = await fetch(apiServerAddress + `/links/${shortCode}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${getAuthToken()}`
+            }
+        });
+        if (!response.ok) {
+            console.warn(response);
+            alert("При удалении ссылки сервер вернул не 2** статус код");
+            return;
+        }
+        deleteUrlFromUI(shortCode);
+    }
+    catch (error) {
+        console.error(error);
+        alert("Ошибка при удалении ссылки");
+    }
+}
+
+async function copyUrl(text) {
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(text);
+        return;
+    }
+
+    const tmp = document.createElement("textarea");
+    tmp.value = text;
+    tmp.setAttribute("readonly", "");
+    tmp.style.position = "absolute";
+    tmp.style.left = "-9999px";
+    document.body.appendChild(tmp);
+    tmp.select();
+    document.execCommand("copy");
+    document.body.removeChild(tmp);
+}
+
 function createTableRow(originalUrl, shortCode, createdAt, expiresAt) {
     const tr = document.createElement("tr");
+    tr.id = shortCode;
 
     tr.innerHTML = `<td class="text-truncate" style="max-width: 260px;">
             <a href="${originalUrl}" class="link-body-emphasis text-decoration-none" target="_blank" rel="noopener noreferrer">
@@ -45,6 +98,9 @@ function createTableRow(originalUrl, shortCode, createdAt, expiresAt) {
                 <button type="button" id="deleteBtn" class="btn btn-sm btn-outline-danger">Удалить</button>
             </div>
         </td>`;
+    
+    tr.querySelector("[id=copyBtn]").addEventListener("click", () => copyUrl(buildShortUrl(shortCode)));
+    tr.querySelector("[id=deleteBtn]").addEventListener("click", () => deleteUrl(shortCode));
     
     return tr;
 }
@@ -88,7 +144,38 @@ async function loadUserUrls() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    showUrlsContainerElement(urlsLoadingIndicator);
-    await loadUserUrls();
+async function loadUserProfile() {
+    try {
+        const response = await fetch(apiServerAddress + "/users/me", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${getAuthToken()}`,
+                "Content-Type": "application/json"
+            }
+        });
+        if (!response.ok) {
+            console.warn(response);
+            profileContainer.loadingIndicator.classList.add("d-none");
+            profileContainer.errorIndicator.classList.remove("d-none");
+            return;
+        }
+        
+        const userData = await response.json();
+        const username = userData.username;
+        const registrationDate = userData.registrationDate;
+        
+        profileContainer.username.textContent = username;
+        profileContainer.registrationDate.textContent = new Date(registrationDate).toLocaleString("ru-RU", {dateStyle: "short"});
+        profileContainer.loadingIndicator.classList.add("d-none");
+        profileContainer.content.classList.remove("d-none");
+    }
+    catch (error) {
+        profileContainer.loadingIndicator.classList.add("d-none");
+        profileContainer.errorIndicator.classList.remove("d-none");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadUserUrls();
+    loadUserProfile();
 });
