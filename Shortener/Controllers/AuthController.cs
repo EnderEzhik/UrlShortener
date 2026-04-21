136bc49f7f0a9a24177fe913b7ace29b3f2198b2
EnderEzhik <Achernov2007@gmail.com>
2026-04-21 22:58:26 +0300

    backend:     change: обновлено логгирование работы с пользователями.

--- Shortener/Controllers/AuthController.cs
+++ Shortener/Controllers/AuthController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Serilog;
+using Serilog.Context;
 using Shortener.Models.DTOs;
 using Shortener.Services;
 
@@ -9,7 +9,7 @@ namespace Shortener.Controllers;
 [Route("api/auth")]
 public class AuthController : ControllerBase
 {
-    private readonly Serilog.ILogger logger = Log.ForContext<AuthController>();
+    private readonly Serilog.ILogger _logger;
     private readonly UserService _userService;
     private readonly JwtService _jwtService;
     
@@ -17,71 +17,86 @@ public class AuthController : ControllerBase
     {
         _userService =  userService;
         _jwtService = jwtService;
+        _logger = Serilog.Log.ForContext<AuthController>();
     }
 
     [HttpPost("register")]
     public async Task<ActionResult<JWTTokenResponse>> Register(UserCreateRequest requestData)
     {
-        logger.Information("Post request for register user");
-
-        if (requestData.Login.Length < 4)
+        using (LogContext.PushProperty("Login", requestData.Login))
         {
-            logger.Warning("Login length is less than 4");
-            return BadRequest(new
+            _logger.Information("Registering new user");
+            
+            if (requestData.Login.Length < 4)
             {
-                message = "Login must contain at least 4 characters"
-            });
-        }
-        if (requestData.Password.Length < 8)
-        {
-            logger.Warning("Password length is less than 8");
-            return BadRequest(new
+                _logger.Warning("Login length is less than 4");
+            
+                return BadRequest(new
+                {
+                    message = "Login must contain at least 4 characters"
+                });
+            }
+        
+            if (requestData.Password.Length < 8)
             {
-                message = "Password must contain at least 8 characters"
-            });
-        }
+                _logger.Warning("Password length is less than 8");
+            
+                return BadRequest(new
+                {
+                    message = "Password must contain at least 8 characters"
+                });
+            }
+    
+            try
+            {
+                var newUser = await _userService.CreateUser(requestData.Login, requestData.Password);
+                
+                _logger.Information("New user registered");
 
-        try
-        {
-            var newUser = await _userService.CreateUser(requestData.Login, requestData.Password);
-            logger.Information("New user created");
-
-            logger.Information("Generating jwt token");
-            var (token, expires) = _jwtService.GenerateJwtToken(newUser);
-            logger.Information("Successfully generated jwt token");
-
-            return new JWTTokenResponse { Token = token, Expires = expires };
-        }
-        catch (ArgumentException)
-        {
-            return Conflict();
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError);
+                var (token, expires) = _jwtService.GenerateJwtToken(newUser);
+    
+                _logger.Debug("Token generated");
+                
+                return new JWTTokenResponse { Token = token, Expires = expires };
+            }
+            catch (ArgumentException)
+            {
+                return Conflict();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error while registering user");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 
     [HttpPost("login")]
     public async Task<ActionResult<JWTTokenResponse>> Login(LoginRequest loginData)
     {
-        logger.Information("Post request for login");
-        try
+        using (LogContext.PushProperty("Login", loginData.Login))
         {
-            var user = await _userService.GetUserByLogin(loginData.Login);
-            if (user is null || user.Password != loginData.Password)
+            _logger.Information("Authorization");
+            
+            try
             {
-                return Unauthorized();
+                var user = await _userService.GetUserByLogin(loginData.Login);
+                if (user is null || user.Password != loginData.Password)
+                {
+                    _logger.Warning("User not found or password is incorrect");
+                    return Unauthorized();
+                }
+                var (token, expires) = _jwtService.GenerateJwtToken(user);
+                
+                _logger.Information("Successful authorization");
+                
+                return new JWTTokenResponse { Token = token, Expires = expires };
             }
-
-            logger.Information("Generating jwt token");
-            var (token, expires) = _jwtService.GenerateJwtToken(user);
-            logger.Information("Successfully generated jwt token");
-            return new JWTTokenResponse { Token = token, Expires = expires };
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError);
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error while authorization");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
--- Shortener/Services/UserService.cs
+++ Shortener/Services/UserService.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Serilog;
 using Shortener.Data;
 using Shortener.Entities;
 
@@ -7,103 +6,94 @@ namespace Shortener.Services;
 
 public class UserService
 {
-    private readonly Serilog.ILogger logger = Log.ForContext<UserService>();
+    private readonly Serilog.ILogger _logger;
     private readonly ApplicationDbContext _db;
     
     public  UserService(ApplicationDbContext db)
     {
         _db = db;
+        _logger = Serilog.Log.ForContext<UserService>();
     }
 
     public async Task<User> CreateUser(string login, string password)
     {
-        logger.Information("Creating new user");
-        
         bool loginAlreadyInUse = await _db.Users.AnyAsync(u => u.Login == login);
         if (loginAlreadyInUse)
         {
-            logger.Warning("User with login {login} already exists", login);
+            _logger.Warning("Login is already in use");
+            
             throw new ArgumentException("Login is already in use");
         }
         
-        User newUser = new User()
+        User user = new User()
         {
             Login = login,
             Password = password
         };
         
-        logger.Information("Saving new user to database");
-        _db.Add(newUser);
-        
         try
         {
+            _db.Add(user);
             await _db.SaveChangesAsync();
-            logger.Information("successfully saved new user to database");
-            return newUser;
+            
+            _logger.Information("User saved to database");
+            
+            return user;
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            logger.Error(e, "Error when saving new user to database");
+            _logger.Error(ex, "Database error while saving user");
             throw;
         }
     }
 
     public async Task<User?> GetUserById(int userId)
     {
-        logger.Information("Searching user by id");
         try
         {
-            User? user = await _db.Users.SingleOrDefaultAsync(u => u.Id == userId);
-            logger.Information("User found in database: {userFound}", user is not null);
-            return user;
+            return await _db.Users.SingleOrDefaultAsync(u => u.Id == userId);
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            logger.Error(e, "Error when searching user by id");
+            _logger.Error(ex, "Database error while getting user by id");
             throw;
         }
     }
 
     public async Task<User?> GetUserByLogin(string login)
     {
-        logger.Information("Searching user by login");
         try
         {
-            User? user = await _db.Users.SingleOrDefaultAsync(u => u.Login == login);
-            logger.Information("User found in database: {userFound}", user is not null);
-            return user;
+            return await _db.Users.SingleOrDefaultAsync(u => u.Login == login);
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            logger.Error(e, "Error when searching user by login");
+            _logger.Error(ex, "Database error while getting user by login");
             throw;
         }
     }
 
     public async Task<bool> DeleteUser(int userId)
     {
-        logger.Information("Searching user to delete");
         User? user = await GetUserById(userId);
         if (user is null)
         {
-            logger.Information("User not found");
+            _logger.Warning("User not found");
             return false;
         }
-        
-        logger.Information("User found");
-        logger.Information("Deleting user");
 
-        _db.Users.Remove(user);
-        
         try
         {
+            _db.Users.Remove(user);
             await _db.SaveChangesAsync();
-            logger.Information("User deleted");
+            
+            _logger.Debug("User deleted");
+            
             return true;
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            logger.Error(e, "Error when deleting User");
+            _logger.Error(ex, "Database error while deleting user");
             throw;
         }
     }
