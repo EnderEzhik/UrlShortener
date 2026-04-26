b044b82c6237f628bb4529fa6331bf10deb12c45
EnderEzhik <Achernov2007@gmail.com>
2026-04-26 13:31:28 +0300

    backend:     change: обновлено логгирование редиректа.

--- Shortener/Controllers/RedirectorController.cs
+++ Shortener/Controllers/RedirectorController.cs
@@ -6,35 +6,44 @@ namespace Shortener.Controllers;
 [ApiController]
 public class RedirectorController : ControllerBase
 {
-    private readonly Serilog.ILogger logger = Serilog.Log.ForContext<RedirectorController>();
+    private readonly Serilog.ILogger _logger;
     private readonly LinksService _urlService;
 
     public RedirectorController(LinksService urlService)
     {
         _urlService = urlService;
+        _logger = Serilog.Log.ForContext<RedirectorController>();
     }
     
     [HttpGet("{shortCode}")]
     public async Task<IActionResult> RedirectFromShortCode(string shortCode)
     {
-        logger.Information("Get Request for redirect from short code");
-        
-        try
+        using (Serilog.Context.LogContext.PushProperty("ShortCode", shortCode))
         {
-            var url = await _urlService.GetCachedShortUrlByShortCodeAsync(shortCode);
-            if (url is null || url.ExpiresAt <= DateTimeOffset.UtcNow)
+            _logger.Information("Redirecting");
+            
+            try
             {
-                return NotFound(new
+                var url = await _urlService.GetCachedShortUrlByShortCodeAsync(shortCode);
+                if (url is null || url.ExpiresAt <= DateTimeOffset.UtcNow)
                 {
-                    message = "Short code not found or expired"
-                });
-            }
+                    _logger.Warning("Short url not found or expired");
 
-            return RedirectPermanent(url.OriginalUrl);
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError);
+                    return NotFound(new
+                    {
+                        message = "Short code not found or expired"
+                    });
+                }
+
+                _logger.Information("Short url found");
+
+                return RedirectPermanent(url.OriginalUrl);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error while redirecting to {shortCode}", shortCode);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
